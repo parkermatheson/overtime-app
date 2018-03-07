f108259f2008e4ab43a0d59c3410879694df13d3
Parker <parkermatheson@gmail.com>
2018-03-06 20:07:22 -0700

    Updated the audit log factory and tested out 6 day default

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ AdminUser.create(email: "admin@test.com",
                  last_name: "Name",
                  phone: '9293251993')
 
-puts "1 Admin User created"
+puts '1 Admin User created'
 
 100.times do |post|
   Post.create!(date: Date.today,
@@ -32,3 +32,9 @@ puts "1 Admin User created"
 end
 
 puts '100 posts have been created'
+
+100.times do |audit_log|
+  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+end
+
+puts ' 100 audit logs have been created'
--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe AuditLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @audit_log = FactoryBot.create(:audit_log)
+  end
+
+  describe 'creation' do
+    it 'can be properly created' do
+      expect(@audit_log).to be_valid
+    end
+  end
 end
