27d30f0587c02d99ab6d6d0619efb5c7f2511bcd
Parker <parkermatheson@gmail.com>
2018-03-07 15:19:27 -0700

    Implemented functional buttons for employee homepage confirmations

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  resources :audit_logs, except: [:new, :edit, :destory]
+  resources :audit_logs, except: [:new, :edit, :destory] do
+    member do
+      get :confirm
+    end
+  end
   
   namespace :admin do
     resources :users
     resources :posts
     resources :admin_users
 
-    root to: 'users#index'
+    root to: "users#index"
   end
 
   resources :posts do
