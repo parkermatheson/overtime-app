3f165d5d122dd8c5f78a3f223a5726da1be346c1
Parker <parkermatheson@gmail.com>
2018-03-09 18:54:27 -0700

    Updated nav to remove components not needed for admins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,15 @@ module ApplicationHelper
   end
 
   def active?(path)
-    "active" if current_page?(path)
+    'active' if current_page?(path)
+  end
+
+  def employee?
+    current_user.type == 'Employee'
+  end
+
+  def admin?
+    admin_types.include?(current_user.type)
   end
 
   def status_label status
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -54,6 +54,8 @@ it 'has a scope so that only post creators can see their posts' do
 
   describe 'new' do
     it 'has a link from the homepage' do
+      employee = Employee.create(first_name: 'Employee', last_name: 'Authorized', email: 'employee@example.com', password: "asdfasdf", password_confirmation: "asdfasdf", phone: "5555555555")
+      login_as(employee, :scope => :user)
       visit root_path
 
       click_link('new_post_from_nav')
